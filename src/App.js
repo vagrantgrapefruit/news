84ae926356b0913b003d49480939b78bec55b736
Thorn <youzijia555@outlook.com>
2018-05-13 23:46:59 +0800

    1

--- src/App.js
+++ src/App.js
@@ -4,15 +4,10 @@ import './App.css';
 
 class App extends Component {
   render() {
+    const helloWorld = 'Welcom to Thorn`s Web'
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <h2>{helloWorld}</h2>
       </div>
     );
   }
