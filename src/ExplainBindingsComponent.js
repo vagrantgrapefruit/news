d9ebcdbcd4a3aaa24aed8c1379a456a8e1341670
Thorn <youzijia555@outlook.com>
2018-05-14 14:49:02 +0800

    2

---
+++ src/ExplainBindingsComponent.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class ExplainBindingsComponent extends Component {
+    constructor() {
+        super();
+        this.onClickMe = this.onClickMe.bind(this);
+    }
+    onClickMe() {
+        console.log(this);
+    }
+    render() {
+        return (
+            <button
+                onClick={this.onClickMe}
+                type="button"
+            >
+                Click Me
+            </button>
+                );
+            }
+}
+
+export default ExplainBindingsComponent;
--- src/index.js
+++ src/index.js
@@ -4,5 +4,11 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import ExplainBindingsComponent from './ExplainBindingsComponent';
+
+
+ReactDOM.render(<ExplainBindingsComponent />, document.getElementById('root'));
+if (module.hot) {
+    module.hot.accept();
+    }
 registerServiceWorker();
